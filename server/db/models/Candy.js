a7261f308038e3da6e45ad93c07d77ad5ae0a9e7
Brian Oblad <a.gray.cloud@gmail.com>
2019-08-23 17:16:54 -0400

    basic api route/dummy data

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -1,6 +1,34 @@
 const Sequelize = require('sequelize');
 const db = require('../database');
 
-module.exports = db.define('candy', {
+const Candy = db.define('candy', {
   // define your model here!
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    // don't know if this is an actual thing or not
+    allowEmpty: false,
+  },
+
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    allowEmpty: false,
+  },
+
+  quantity: {
+    type: Sequelize.INTEGER,
+  },
+
+  imageUrl: {
+    type: Sequelize.STRING,
+  },
 });
+
+Candy.beforeValidate(instance => {
+  if (instance.quantity && instance.quantity > 10) {
+    instance.quantity = 10;
+  }
+});
+
+module.exports = Candy;
